adfc68b1913839a9b366c8ac4c95d54e8a0668ab
tribui1912 <56878689+tribui1912@users.noreply.github.com>
2023-08-23 19:52:11 -0700

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,36 @@
+print("Hello World!\n")
+
+p = 25
+q = 10
+r = p * q
+print(r)
+print("\n")
+
+r = p / q
+print(r)
+print("\n")
+
+r = p // q
+print(r)
+print("\n")
+
+r = p % q
+print(r)
+print("\n")
+
+r = pow(5,3,7)
+print(r)
+print("\n")
+
+p = "This is string number 1"
+q = 'This is string number 2'
+print(p)
+print(q)
+print("\n")
+
+p = """this
+is
+multiple
+line
+string"""
+print(p)
